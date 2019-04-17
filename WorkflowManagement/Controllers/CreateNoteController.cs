1dcfb51b28e7224d4962484623c38a49476214c2
fndlovu48 <fndlovu48@gmail.com>
2019-04-17 09:04:55 +0200

    j

---
+++ WorkflowManagement/Controllers/CreateNoteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkflowManagement.Controllers
+{
+    public class CreateNoteController : Controller
+    {
+        // GET: CreateNote
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("CreateNote");
+        }
+    }
+}
